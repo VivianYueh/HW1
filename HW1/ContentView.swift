ae999b6dc3293f31c44a6f024e96691d9486fd26
louyu0510 <louyu50310@gmail.com>
2023-03-10 15:58:30 +0800

    Initial Commit

---
+++ HW1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW1
+//
+//  Created by User13 on 2023/3/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW1/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  HW1
+//
+//  Created by User13 on 2023/3/10.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
